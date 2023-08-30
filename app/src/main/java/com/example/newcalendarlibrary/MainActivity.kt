386ef38776251be1a13d5dee10b54de3a663b8e7
Ravin De Silva <ravin.de.silva@stud.fh-campuswien.ac.at>
2023-08-30 10:53:15 +0200

    new calendar library & create event screen

--- app/src/main/java/com/example/newcalendarlibrary/MainActivity.kt
+++ app/src/main/java/com/example/newcalendarlibrary/MainActivity.kt
@@ -1,92 +1,20 @@
 package com.example.newcalendarlibrary
 
 import android.os.Bundle
-import android.support.v4.os.IResultReceiver2.Default
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import com.example.newcalendarlibrary.navigation.Navi
+import com.example.newcalendarlibrary.calendar.CalendarScreen
 import com.example.newcalendarlibrary.ui.theme.NewCalendarLibraryTheme
-import com.kizitonwose.calendar.core.CalendarDay
-import com.kizitonwose.calendar.core.DayPosition
-import java.time.DayOfWeek
-import java.time.format.TextStyle
-import java.util.*
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             NewCalendarLibraryTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Navi()
-                }
+                //You can Comment/unComment below Composable functions to test it on Emulator or device
+               //AddEventScreen()
+                CalendarScreen()
             }
-        }
-    }
-}
-@Composable
-fun MonthHeader(daysOfWeek: List<DayOfWeek>) {
-
-}
-
-
-
-@Composable
-fun Day(day: CalendarDay, isSelected: Boolean, onClick: (CalendarDay) -> Unit) {
-    Box(
-        modifier = Modifier
-            .aspectRatio(0.5f)
-            .clip(CircleShape)
-            .background(color = if (isSelected) Color.LightGray else Color.Transparent)
-            .padding(horizontal = 10.dp)
-            .padding(vertical = 10.dp)
-            .width(300.dp)
-            .clickable(
-                enabled = day.position == DayPosition.MonthDate,
-                onClick = { onClick(day)}
-            ),// This is important for square sizing!
-        contentAlignment = Alignment.Center
-    ) {
-        Text(text = day.date.dayOfMonth.toString(),
-        color = if(day.position == DayPosition.MonthDate) Color.White else Color.DarkGray)
-         }
-}
-
-
-@Composable
-fun DaysOfWeekTitle(daysOfWeek: List<DayOfWeek>) {
-    Row(modifier = Modifier.fillMaxWidth()) {
-        for (dayOfWeek in daysOfWeek) {
-            Text(
-                modifier = Modifier
-                    .weight(1f)
-                    .padding(vertical = 70.dp)
-                    .padding(horizontal = 1.dp),
-                    //.padding(top = 1.dp),
-
-
-                textAlign = TextAlign.Center,
-                text = dayOfWeek.getDisplayName(TextStyle.SHORT, Locale.getDefault()),
-            )
         }
     }
 }
